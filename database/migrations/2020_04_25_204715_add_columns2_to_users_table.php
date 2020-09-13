010fc9b09897e5da0317bd6f9a0b65f8b87701a2
welby1 <welby@mail.ru>
2020-09-13 12:51:22 +0300

    fix migration double name

--- database/migrations/2020_04_25_204715_add_columns2_to_users_table.php
+++ database/migrations/2020_04_25_204715_add_columns2_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnsToUsersTable extends Migration
+class AddColumns2ToUsersTable extends Migration
 {
     /**
      * Run the migrations.
